e8d43dd550ba01fb9cef17906244c911fdcf917d
Mariana Licona <m.licona@abtis.de>
2021-06-09 22:34:25 +0200

    Agregar archivos de proyecto.

---
+++ EMPLEADO.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaryCalculationWithConstructors
+{
+    class EMPLEADO
+    {
+        // Declaracion de atributos
+
+        private double SM = 0.0; //Salario mensual x input
+        private double BM = 0.0;// Bono mensual definido como constante
+
+        // Procedemos con el acceso a la modificacion los datos miembro
+        // mediante la utilización de las sentencias get y set
+
+        //Acceso a salario
+        public double Salario
+        {
+            //Descriptores de acceso
+            get
+            {
+                return SM;
+            }
+            set
+            {
+                //Estatua SM en 0.0 si el valor input es negativo
+                if (value < 0)
+                {
+                    SM = 0.0;
+                }
+                else
+                {
+                    // Si algo diferente pasa se toma el valor
+                    SM = value;
+                }
+            }
+        }
+
+        // Acceso a bono mensual
+        public double BonoMensual
+        {
+            get
+            {
+                return BM;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    BM = 0.0;
+                }
+                else
+                {
+                    BM = value;
+                }
+            }
+        }
+
+        //Ahora aumentamos el salario el 10 % con un metodo
+        // AS aumento salario 
+        public double AS(double SalarioBasico)
+        {
+            if (SalarioBasico > 2500 && SalarioBasico <= 5000)
+            {
+                double CalculoAumento = SalarioBasico * 0.10;
+                SalarioBasico = CalculoAumento + SalarioBasico;
+            }
+            return SalarioBasico;
+        }
+
+        //Ahora calculemos el salario anual
+
+        public double SalarioAno(double salarioAnualUnoDoce)
+        {
+            double SalarioAno = salarioAnualUnoDoce * 12;
+            salarioAnualUnoDoce = SalarioAno;
+            return salarioAnualUnoDoce;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SalaryCalculationWithConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esta es la evolucion del software de nomina");
+            //Instanciacion de la clase EMPLEADO
+            EMPLEADO Empleado = new EMPLEADO();
+            Console.WriteLine("Bienvenido a SalarySoft");
+            Console.WriteLine("Siga los pasos indicados para el calculo de su $alario Anual $$$");
+            Console.WriteLine("1- Ingrese su salario mensual");
+            var SalarioInputUser = Console.ReadLine();
+            double SalarioParced = double.Parse(SalarioInputUser);
+            //SM SALARIO MENSUAL
+            var SM = SalarioParced;
+            //Dot notation para acceder al atributo SALARIO de la clase Empleado
+            Empleado.Salario = SM;
+            var SCM = Empleado.AS(SM);
+            var SAN = Empleado.SalarioAno(SM);
+            //Imprime los resultados del calculo
+            //mientras estamos en la clase main
+            Console.WriteLine("Su salario mensual es de:" + SM);
+            Console.WriteLine("Su salario con aumento extraordinario del 10% es de:" + SCM);
+            Console.WriteLine("Su salario anual es de:" + SAN);
+        }
+    }
+}
